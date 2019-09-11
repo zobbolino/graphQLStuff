d98fc6684ad6d5eb1a4860660ead27538ec1695d
Leo Friel <leo.friel@btinternet.com>
2019-09-11 10:08:05 +0100

    Edits to my query  Please enter the commit message for your changes. Lines starting

--- query_repo.js
+++ query_repo.js
@@ -1,9 +1,24 @@
+#query the named repository for details of the stargazers ("likes")
 {
   repository(owner: "zobbolino", name:"graphQLStuff") {
+    id
     shortDescriptionHTML
     createdAt
+    commitComments(first: 10) {
+      edges {
+        node {
+          id
+          body
+          bodyText
+          bodyHTML
+        }
+      }
+    }
+    viewerHasStarred
     stargazers(first:5){
+      totalCount
       nodes{
+        id
         name
         login
       }
